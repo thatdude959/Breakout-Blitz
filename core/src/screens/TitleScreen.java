1fb50eb600ae47cdc35687fcd2a45c95309e13f5
thatdude959 <christophercheung6@gmail.com>
2024-06-05 22:13:14 -0400

    test

--- core/src/screens/TitleScreen.java
+++ core/src/screens/TitleScreen.java
@@ -2,10 +2,14 @@ package screens;
 
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.game.BreakoutBlitz;
 
 public class TitleScreen extends ScreenAdapter {
     BreakoutBlitz game;
+    private Skin skin;
+    private Stage stage;
 
     public TitleScreen(BreakoutBlitz game) {
         this.game = game;
@@ -29,8 +33,7 @@ public class TitleScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
-        game.font.draw(game.batch, "Click the circle to win.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
+        game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .45f, Gdx.graphics.getHeight() * .75f);
         game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
         game.batch.end();
     }
